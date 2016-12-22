d75fa8b91c738cbcd164d7cde2e5cb882d65f370
rookie211 <hll211@163.com>
2016-12-22 15:03:12 +0000

    add learning-01.py

---
+++ Learning/Learning-01.py
@@ -0,0 +1,18 @@
+#this is my rocks python repository.
+#python version: 2.7.12
+
+print("hello world")
+print ('this is','my rocks','python','example')
+print(100 + 200)
+print (100 + 200)
+print ("input over.")
+#name = input("input your name:")
+#print ('hello', name)
+print(r'\\\\\t\\n\\\\')
+print('''line 1
+line 2
+line 3''')
+if 3 > 2:
+    print("3 > 2")
+if 3 < 1:
+    print("3 < 1")
